a4d0d9ef5925f2090585a1f5aa4a46f86fb1378d
mayukuse24 <vinaypoll@gmail.com>
2020-02-09 19:36:04 -0600

    Created and added client for algorithm

---
+++ tutorial/src/app/Client.java
@@ -0,0 +1,123 @@
+package app;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.time.*;
+
+public class Client {
+    String id;
+    List<Node> serverList = new ArrayList<Node>();
+
+    public static class Node {
+        String id;
+        String ip;
+        int port;
+
+        public Node(String Id, String Ip, int p) {
+            this.id = Id;
+            this.ip = Ip;
+            this.port = p;
+        }
+    }
+
+    public Client(String Id) {
+        this.id = Id;
+    }
+
+    public void loadConfig(String fileName) {
+        try {
+            BufferedReader inputBuffer = new BufferedReader(new FileReader(fileName));
+            String line;
+            String[] params;
+
+            System.out.println("Loading servers from config file");
+
+            while ((line = inputBuffer.readLine()) != null) {
+                params = line.split(" ");
+
+                System.out.println(String.format("Found server %s, ip=%s, port=%s", params[0], params[1], params[2]));
+
+                this.serverList.add(new Node(params[0], params[1], Integer.parseInt(params[2])));
+            }
+
+            inputBuffer.close();
+        }
+        catch (Exception e) {
+            System.out.println(String.format("Could not load config from file: %s", fileName));
+        }  
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Client Started");
+
+        PrintWriter out;
+        BufferedReader in;
+        String appendRequest;
+        String[] fileList = {"f1", "f2", "f3", "f4"};
+
+        Random rand = new Random();
+
+        Client client = new Client(args[0]);
+
+        // Get list of available file servers from config.txt file
+        client.loadConfig("config.txt");
+
+        System.out.println(client.serverList);
+
+        Socket echoSocket = null;
+
+        for (int msgNo = 1; msgNo <= 10; msgNo++) {
+            Thread.sleep(rand.nextInt(1000)); // Sleep for atmost 1 second
+
+            // Select random server for sending append request
+            int serverIndex = rand.nextInt(client.serverList.size());
+            Node selectedServer = client.serverList.get(serverIndex);
+
+            echoSocket = new Socket(selectedServer.ip, selectedServer.port);
+
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+    
+            in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+
+            // Pick a random file to append message
+            String fileName = fileList[rand.nextInt(fileList.length)];
+
+            Instant instant = Instant.now();
+
+            appendRequest = String.format(
+                "%s requests:client %s message #%s -- server %s: for file #%s at time: %s",
+                client.id,
+                client.id,
+                msgNo,
+                selectedServer.id,
+                fileName,
+                instant.toEpochMilli()
+            );
+
+            System.out.println(appendRequest);
+
+            try {
+                out.println(appendRequest);
+            }
+            catch (Exception e) {
+                System.out.println(String.format("%s receives a failure from %s", client.id, selectedServer.id));
+            }                 
+
+            String response = in.readLine();
+
+            if (response.equals("ACK")) {
+                System.out.println(
+                    String.format("%s receives a successful ack from %s", client.id, selectedServer.id)
+                );
+            }
+            else {
+                System.out.println(String.format("%s receives a failure from %s", client.id, selectedServer.id));
+            }
+        }
+
+        System.out.println("Exiting Client");
+
+        echoSocket.close();
+    }
+}
